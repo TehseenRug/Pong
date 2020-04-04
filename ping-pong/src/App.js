fa1b06f152143fac07fcbb488ed40cba57f4199f
TehseenRug <tehseen.rug@gmail.com>
2020-04-04 16:49:01 +0200

    some very basic styling of header and body

--- ping-pong/src/App.js
+++ ping-pong/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import {LandingPage} from "./components/LandingPage";
 import {GamePage} from "./components/GamePage";
 
@@ -8,24 +8,20 @@ function App() {
     return (
         <Router>
             <div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <ul className="navbar-nav mr-auto">
-                        <li><Link to={'/'} className="nav-link"> Home </Link></li>
-                        <li><Link to={'/pingpong'} className="nav-link">Let's play</Link></li>
+                <h1 className="title">Pong</h1>
+                <nav className="header">
+                    <ul>
+                    <li><Link to={'/'}> Home </Link></li>
+                    <li><Link to={'/pingpong'}>Let's play</Link></li>
                     </ul>
                 </nav>
-                <hr />
+                <hr/>
                 <Switch>
-                    <Route exact path='/' component={LandingPage} />
-                    <Route path='/pingpong' component={GamePage} />
+                    <Route exact path='/' component={LandingPage}/>
+                    <Route path='/pingpong' component={GamePage}/>
                 </Switch>
             </div>
         </Router>
-        // <div className="App">
-        //     <header className="App-header">
-        //         <GameBoard value="this is the value 2" />
-        //     </header>
-        // </div>
     );
 }
 
--- ping-pong/src/components/GameBoard.js
+++ ping-pong/src/components/GameBoard.js
@@ -1,28 +1,40 @@
 import * as React from "react";
 
 let interval = 0;
-export class GameBoard extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {time: 0}
-    }
 
-    componentDidMount(){
-        interval = window.setInterval(() => this.tick(), 1000);
-    }
+export class GameBoard extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {time: 0}
+    // }
 
-    componentWillUnmount(){
-        clearInterval(interval);
-    }
+    // componentDidMount() {
+    //     interval = window.setInterval(() => this.tick(), 1000);
+    // }
+    //
+    // componentWillUnmount() {
+    //     clearInterval(interval);
+    // }
 
-    tick() {
-        this.setState({
-            time: this.state.time + 1
-        });
-    }
+    // tick() {
+    //     this.setState({
+    //         time: this.state.time + 1
+    //     });
+    // }
 
     render() {
-        return (<div>This should be the game board {this.props.value}</div>);
+        return (
+            <div>
+                <button onClick={this.startGame}>Start Game</button>
+                <div>
+                    This should be the game board!
+                    {/*{this.props.value}*/}
+                </div>
+            </div>);
+    }
+
+    startGame() {
+        console.log("Game started!");
     }
 
 
