b644d8aa77c4b8a350f37bb603ce11fb3e919666
TehseenRug <tehseen.rug@gmail.com>
2020-04-05 11:32:01 +0200

    pass game state to board

--- ping-pong/src/components/GameBoard.js
+++ ping-pong/src/components/GameBoard.js
@@ -1,40 +1,25 @@
 import * as React from "react";
 
-let interval = 0;
 
 export class GameBoard extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {time: 0}
-    // }
-
-    // componentDidMount() {
-    //     interval = window.setInterval(() => this.tick(), 1000);
-    // }
-    //
-    // componentWillUnmount() {
-    //     clearInterval(interval);
-    // }
-
-    // tick() {
-    //     this.setState({
-    //         time: this.state.time + 1
-    //     });
-    // }
 
     render() {
-        return (
-            <div>
-                <button onClick={this.startGame}>Start Game</button>
+        if (this.props.gameActive) {
+            return (
                 <div>
-                    This should be the game board!
-                    {/*{this.props.value}*/}
-                </div>
-            </div>);
-    }
-
-    startGame() {
-        console.log("Game started!");
+                    <div>
+                        This should be the game board in active mode!
+                    </div>
+                </div>);
+        }
+        else{
+            return (
+                <div>
+                    <div>
+                        This should be the game board in paused mode!
+                    </div>
+                </div>);
+        }
     }
 
 
--- ping-pong/src/components/GamePage.js
+++ ping-pong/src/components/GamePage.js
@@ -2,13 +2,23 @@ import {GameBoard} from "./GameBoard";
 import React from "react";
 
 export class GamePage extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {gameActive: false}
+    }
+
+    toggleGameActive = () => {
+        this.setState(prevState => ({gameActive: !prevState.gameActive}));
+    };
+
     render() {
         return (
             <div>
                 <h2>Let's play Ping Pong</h2>
-                <GameBoard value="this is the value 2"/>
-
-            </div>
-        )
+                <button onClick={this.toggleGameActive}>Start Game</button>
+                <GameBoard gameActive={this.state.gameActive}/>
+            </div>);
     }
+
 }
