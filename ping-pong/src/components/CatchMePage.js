56a8fb97f93715da6e38f0e4f3cf8c095606df43
Michael Haslauer <michael.haslauer@gmx.de>
2020-04-11 14:18:26 +0200

    render catch me fields

--- ping-pong/src/components/CatchMePage.js
+++ ping-pong/src/components/CatchMePage.js
@@ -13,6 +13,22 @@ export class CatchMePage extends React.Component {
         return (
             <div className='catchme'>
                 <h2> Catch me if you can.</h2>
+                <div className='catchmeboard'> {this.renderFields()} </div>
             </div>);
+    }
+
+    renderFields(){
+        let fields = [];
+        let i;
+        for (i = 0; i < 64; i++) {
+            fields.push(this.renderField(i));
+        }
+        return fields;
+    }
+
+    renderField(id){
+        return (
+            <div className='catchmefield'> {id} </div>
+        );
     }
 }
